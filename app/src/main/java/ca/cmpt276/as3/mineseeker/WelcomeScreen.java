c50f4ce2f56d1343dd264534cfb02dfd27086c23
nargisgh <56950238+nargisgh@users.noreply.github.com>
2020-06-19 18:38:50 -0700

    Spinning image animation

--- app/src/main/java/ca/cmpt276/as3/mineseeker/WelcomeScreen.java
+++ app/src/main/java/ca/cmpt276/as3/mineseeker/WelcomeScreen.java
@@ -3,12 +3,27 @@ package ca.cmpt276.as3.mineseeker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class WelcomeScreen extends AppCompatActivity {
-
+    Animation ranimation;
+    ImageView myImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        myImage = findViewById(R.id.anim);
+        myImage.setImageResource(R.drawable.asteroidfalling);
+
+        rotateAnimation();
+
+    }
+
+    private void rotateAnimation() {
+        ranimation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+        myImage.startAnimation(ranimation);
+        //https://www.youtube.com/watch?v=goVoYf2qie0
     }
 }
