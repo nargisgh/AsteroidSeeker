acba10cb6def76b437baade6e0b57ccb7bf72077
nargisgh <56950238+nargisgh@users.noreply.github.com>
2020-06-20 15:01:10 -0700

    Safe delete java class to output a better Welcome Screen
    (Under the name MainActivity )

--- app/src/main/java/ca/cmpt276/as3/mineseeker/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/mineseeker/MainActivity.java
@@ -6,17 +6,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import ca.cmpt276.as3.mineseeker.ui.WelcomeScreen;
+import ca.cmpt276.as3.mineseeker.ui.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
+    Animation ranimation;
+    ImageView myImage;
+    Button skip;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, WelcomeScreen.class);
-        startActivity(intent);
+        myImage = findViewById(R.id.anim);
+        myImage.setImageResource(R.drawable.asteroidfalling);
+        rotateAnimation();
+
+        skipBtn();
+    }
+    private void skipBtn() {
+        skip = findViewById(R.id.skipbtn);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void rotateAnimation() {
+        ranimation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+        myImage.startAnimation(ranimation);
+        //https://www.youtube.com/watch?v=goVoYf2qie0
     }
 }
