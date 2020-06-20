8b8be858dc063e90275b9a21918c7497d7d4cc5d
nargisgh <56950238+nargisgh@users.noreply.github.com>
2020-06-19 17:35:55 -0700

    setting up skip button

--- app/src/main/java/ca/cmpt276/as3/mineseeker/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/mineseeker/MainActivity.java
@@ -1,12 +1,17 @@
 package ca.cmpt276.as3.mineseeker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
