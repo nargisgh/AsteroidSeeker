c0e993ae33d01a369cdc4651e09ffe3005afa0d7
nargisgh <56950238+nargisgh@users.noreply.github.com>
2020-06-21 20:16:23 -0700

    Erase button defined in the options class

--- app/src/main/java/ca/cmpt276/as3/mineseeker/ui/Options.java
+++ app/src/main/java/ca/cmpt276/as3/mineseeker/ui/Options.java
@@ -3,15 +3,17 @@ package ca.cmpt276.as3.mineseeker.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import ca.cmpt276.as3.mineseeker.R;
 
 public class Options extends AppCompatActivity {
-
+    Button eraseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
     }
 }
